84d5daa548fbb99297b16535ec7c56ac83b91e58
Jesse Loftus <jesse.loftus@gmail.com>
2018-08-22 15:53:36 -0700

    built table schemas and relationships

---
+++ models/order.js
@@ -0,0 +1,10 @@
+module.exports = function (sequelize, DataTypes) {
+    const Order = sequelize.define("Order", {
+        olID: DataTypes.INTEGER,
+        usSupervisorID: DataTypes.INTEGER,
+    });
+    Order.associate = function (models) {
+        Order.hasMany(models.OrderLine);
+    };
+    return Order;
+};
--- models/product.js
+++ models/product.js
@@ -1,14 +1,14 @@
 module.exports = function (sequelize, DataTypes) {
-    var Products = sequelize.define('products', {
-        prodCategory: DataTypes.STRING,
-        prodName: DataTypes.STRING,
-        prodOnHand: DataTypes.INTEGER,
-        prodPAR: DataTypes.INTEGER,
-        prodPrice: {
-            type: DataTypes.DECIMAL,
-            precision: 2
-        },
-        prPhoto: DataTypes.TEXT
-    });
-    return Products;
+  const Product = sequelize.define('Product', {
+    prodCategory: DataTypes.STRING,
+    prodName: DataTypes.STRING,
+    prodOnHand: DataTypes.INTEGER,
+    prodPAR: DataTypes.INTEGER,
+    prodPrice: {
+      type: DataTypes.DECIMAL,
+      precision: 2
+    },
+    prPhoto: DataTypes.TEXT
+  });
+  return Product;
 };
