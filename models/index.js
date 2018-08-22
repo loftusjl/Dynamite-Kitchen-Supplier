88f433bf784b4656d4f514f1f4c4de8c01317672
Jesse Loftus <jesse.loftus@gmail.com>
2018-08-22 12:57:11 -0700

    removed example from models. built initial models files. structured models/product.js

--- models/index.js
+++ models/index.js
@@ -12,9 +12,9 @@ if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
+    process.env.DB_DATABASE,
+    process.env.DB_USER,
+    process.env.DB_PASS,
     config
   );
 }
