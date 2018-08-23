c61d2db530406f10e53b4b60360ba8b4ae153257
Jesse Loftus <jesse.loftus@gmail.com>
2018-08-22 17:53:22 -0700

    updated eslintrc. changed var to const in models

--- models/orderLine.js
+++ models/orderLine.js
@@ -1,14 +1,14 @@
-module.exports = function (sequelize, DataTypes) {
-    const OrderLine = sequelize.define('OrderLine', {
-        olQuantity: DataTypes.INTEGER,
-        prodID: DataTypes.INTEGER,
-    });
-    OrderLine.associate = function (models) {
-        OrderLine.belongsTo(models.User, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-    };
-    return OrderLine;
+module.exports = function(sequelize, DataTypes) {
+	const OrderLine = sequelize.define('OrderLine', {
+		olQuantity: DataTypes.INTEGER,
+		prodID: DataTypes.INTEGER
+	});
+	OrderLine.associate = function(models) {
+		OrderLine.belongsTo(models.User, {
+			foreignKey: {
+				allowNull: false
+			}
+		});
+	};
+	return OrderLine;
 };
--- models/user.js
+++ models/user.js
@@ -1,20 +1,20 @@
 module.exports = function (sequelize, DataTypes) {
-    const User = sequelize.define('User', {
-        usName: DataTypes.STRING,
-        usPhone: {
-            type: DataTypes.SMALLINT,
-            validate: {
-                len: [10,15]
-            }
-        },
-        usStreet: DataTypes.STRING,
-        usCity: DataTypes.STRING,
-        usState: DataTypes.STRING,
-        usZip: {
-            type: DataTypes.INTEGER,
-            len: [5]
-        },
-        usRole: DataTypes.STRING
-    });
-    return User;
+	const User = sequelize.define('User', {
+		usName: DataTypes.STRING,
+		usPhone: {
+			type: DataTypes.SMALLINT,
+			validate: {
+				len: [10,15]
+			}
+		},
+		usStreet: DataTypes.STRING,
+		usCity: DataTypes.STRING,
+		usState: DataTypes.STRING,
+		usZip: {
+			type: DataTypes.INTEGER,
+			len: [5]
+		},
+		usRole: DataTypes.STRING
+	});
+	return User;
 };
