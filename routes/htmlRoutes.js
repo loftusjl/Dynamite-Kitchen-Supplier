a1bd49d803fb3928248210697945bf5b63e53c15
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2018-08-27 21:29:23 -0700

    update handlebars deleted duplicate files

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -18,23 +18,6 @@ module.exports = function(app) {
 			});
 		});
 	});
-	// Load superUser page
-	app.get('/superUser', function(req, res) {
-		db.Product.findAll({}).then(function(dbProduct) {
-			res.render('superUser', {
-				product: dbProduct
-			});
-		});
-	});
-	// Load supervisor page
-	app.get('/supervisor', function(req, res) {
-		//! change to only rendering. Reference API routes for actual data query
-		db.Product.findAll({}).then(function(dbProduct) {
-			res.render('supervisor', {
-				product: dbProduct
-			});
-		});
-	});
 
 	// Load product page and pass in an product by id
 	app.get('/order', function(req, res) {
