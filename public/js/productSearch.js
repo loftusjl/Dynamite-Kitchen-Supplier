282ac6e482e14d726fa22707d65aa5e4d0de2f20
JesseL <JESSE.LOFTUS@GMAIL.COM>
2018-08-28 18:34:38 -0700

    product search now works

--- public/js/productSearch.js
+++ public/js/productSearch.js
@@ -1,9 +1,8 @@
-document.addEventListener('DOMContentLoaded', function(event) { 
-	event.preventDefault();
-	console.log(search);
+document.addEventListener('DOMContentLoaded', function() { 
 	let searchForm = document.getElementById('searchForm');
-	searchForm.addEventListener('submit', function() {
+	searchForm.addEventListener('submit', function(event) {
 		let search = document.getElementById('product-text').value;
+		event.preventDefault();
 		console.log('submit clicked: ', search);
 		window.location.replace('/api/products/search/' + search);
 		return false;
