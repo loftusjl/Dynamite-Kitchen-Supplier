b78a6ac7dfd40e40e11c5d9d0857e2eeeb24e91d
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2018-09-02 00:12:34 -0700

    updated order, now posts order. but does not wipe old in orderline on update

---
+++ public/js/orderSubmit.js
@@ -0,0 +1,20 @@
+function submitOrder(data) {
+    if ('#order-list') {
+
+        $.ajax({
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            type: 'POST',
+            url: '/api/supervisor/order',
+            data: JSON.stringify(data)
+        }).then(function (event) {
+
+            window.location.replace('/api/orders');
+        })
+    } else {
+        alert('No items in que!')
+    }
+}
+
+$('#submit-order').on('click', submitOrder)
--- public/js/products.js
+++ public/js/products.js
@@ -96,6 +96,8 @@ var handleDeleteBtnClick = function () {
 
 M.AutoInit();
 
+
+
 document.addEventListener('DOMContentLoaded', function () {
 	var elems = document.querySelectorAll('.fixed-action-btn');
 	var instances = M.FloatingActionButton.init(elems, {
