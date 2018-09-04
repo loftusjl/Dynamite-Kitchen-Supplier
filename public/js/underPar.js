86dacf1b628a2264b43b86a6a733e3bc2cd499ba
JesseL <JESSE.LOFTUS@GMAIL.COM>
2018-09-03 19:01:03 -0700

    moved unitofissue to products

--- public/js/underPar.js
+++ public/js/underPar.js
@@ -1,21 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
 	// listen for textbox change
+	let prodName = document.getElementsByClassName('prodName');
+	for (i=0; i < prodName.length; i++) {
+		if (prodName) {console.log('Product ID ', prodName[i].dataset.prodid);}
+	}
 	let qtyReq = document.getElementsByClassName('qtyRequested');
 	for (i=0; i < qtyReq.length; i++) {
 		let ordId = qtyReq[i].id;
 		qtyReq[i].addEventListener('change', function() {
 			newVal = getVal(ordId);
-			$.ajax({
-				headers: {'Content-Type':'application/json'},
-				type: 'PUT',
-				url: '/api/order/lineitem/' + this.id,
-				data: JSON.stringify({
-					olQuantity: newVal
-				})
-			})
-				.then(function() {
-					window.location.replace('/order');
-				});
+			console.log('prodID: ', getElementById(`order-${this.id}`).dataset.prodid);
+			// $.ajax({
+			// 	headers: {'Content-Type':'application/json'},
+			// 	type: 'PUT',
+			// 	url: '/api/order/lineitem/' + this.id,
+			// 	data: JSON.stringify({
+			// 		olQuantity: newVal,
+			// 		prodID: getElementById(`order-${this.id}`).dataset.prodid,
+
+			// 	})
+			// })
+			// 	.then(function() {
+			// 		window.location.replace('/underpar');
+			// 	});
 		});
 	}
 });
