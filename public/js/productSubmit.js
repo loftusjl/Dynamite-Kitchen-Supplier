8a09a14d429134bbd54f40ba1b93f317713be190
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2018-09-05 14:05:55 -0700

    update

--- public/js/productSubmit.js
+++ public/js/productSubmit.js
@@ -1,20 +1,20 @@
+
 $('#submit-product').on('click', submitProduct);
 
-
-
 function submitProduct(data) {
-    if (this.id) {
-        $.ajax({
-            headers: {
-                'Content-Type': 'application/json'
+	console.log('this.id ' + data)
+    if ('#product-to-submit') {
+		$.ajax({
+			headers: {
+				'Content-Type': 'application/json'
 			},
 			type: 'POST',
 			url: '/api/order/lineitem',
 			data: JSON.stringify(data)
 		}).then(function (event) {
-            console.log('this.id' + this.id)
-		
-			// window.location.replace('/order');
+			
+			console.log('this.id' + event)
+			window.location.replace('/basicuser');
 		});
 	} else {
 		alert('No items in que!');
